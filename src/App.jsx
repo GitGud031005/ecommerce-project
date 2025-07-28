9d925c1c3abfa59d1e75aa1b852d72597c92ec54
GitGud031005 <phuc.nguyen0310@hcmut.edu.vn>
2025-07-28 23:52:01 +0700

    feat: React CheckoutPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
+import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
+import { CheckoutPage } from './pages/CheckoutPage'
 
 function App() {
 
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path='checkout' element={<CheckoutPage />} />
+    </Routes>
   )
 }
 
